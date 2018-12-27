b067ac84924acc1f9daf533befda40bfb24ecc5f
Steven Vroonland <steven.vroonland@ordina.nl>
2018-12-27 10:56:32 +0100

    Cleanup, documentation and make the ZeroMQ source non-blocking

--- src/main/scala/nl/codestar/data/ZeroMqSource.scala
+++ src/main/scala/nl/codestar/data/ZeroMqSource.scala
@@ -5,17 +5,38 @@ import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.stream.OverflowStrategy
 import akka.stream.scaladsl.Source
-import nl.codestar.data.ZeroMqSourceActor.Stop
 import org.zeromq.{ZMQ, ZMsg}
 
 import scala.annotation.tailrec
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.{FiniteDuration, _}
+import akka.actor.typed.scaladsl.adapter._
 
-object ZeroMqSourceActor {
+object ZeroMqSource {
+
+  /**
+    * Creates a [[Source]] with messages from ZeroMQ
+    *
+    * @param url URL of the ZeroMQ server
+    * @param port Port of the ZeroMQ server
+    * @param envelopes Envelopes to subscribe to
+    * @param pollInterval Interval to check for new messages
+    * @param bufferSize Amount of messages the source can buffer before failing
+    * @return Source that materializes to a function that will close the ZeroMQ connection
+    */
+  def apply(url: String, port: Int, envelopes: Seq[String], pollInterval: FiniteDuration = 50.millis, bufferSize: Int = 100)(implicit ec: ExecutionContext,
+                                                                                                                             actorSystem: ActorSystem): Source[ZMsg, () => Unit] =
+    Source.actorRef(bufferSize, OverflowStrategy.fail).mapMaterializedValue { sourceActor =>
+      val actor = actorSystem.spawnAnonymous(behavior(url, port, envelopes, pollInterval, sourceActor.toTyped[ZMsg]))
+      () =>
+        actor ! Stop
+    }
 
   /**
     * Actor that polls ZeroMQ for available messages and forwards them to another actor for further processing
+    *
+    * The ZMQ server is polled periodically according to the `pollInterval`. When more messages are available
+    * immediately, they are read immediately.
     *
     * @param url
     * @param port
@@ -24,55 +45,42 @@ object ZeroMqSourceActor {
     * @param receiver
     * @return
     */
-  def behavior(url: String, port: Int, envelopes: Seq[String], pollInterval: FiniteDuration, receiver: ActorRef[ZMsg]): Behavior[Command] = Behaviors.setup { ctx =>
-    Behaviors.withTimers { timers =>
-      val zmqContext = ZMQ.context(1)
-      val socket     = zmqContext.socket(ZMQ.SUB)
-      val address    = s"tcp://$url:$port"
+  private def behavior(url: String, port: Int, envelopes: Seq[String], pollInterval: FiniteDuration, receiver: ActorRef[ZMsg], nrIoThreads: Int = 1): Behavior[Command] =
+    Behaviors.setup { ctx =>
+      Behaviors.withTimers { timers =>
+        val zmqContext = ZMQ.context(nrIoThreads)
+        val socket     = zmqContext.socket(ZMQ.SUB)
+        val address    = s"tcp://$url:$port"
 
-      socket.connect(address)
-      println(s"Connected to ${address} and envelopes ${envelopes.mkString(",")}")
-      envelopes.map(_.getBytes).foreach(socket.subscribe)
+        socket.connect(address)
+        envelopes.map(_.getBytes).foreach(socket.subscribe)
 
-      @tailrec
-      def receiveAllAvailable(): Unit = {
-        val msg = Option(ZMsg.recvMsg(socket)) // , ZMQ.DONTWAIT | ZMQ.NOBLOCK))
-        msg.foreach(receiver ! _)
-        if (msg.isDefined) {
-          receiveAllAvailable()
-        } else ()
-      }
+        @tailrec
+        def receiveAllAvailable(): Unit = {
+          val msg = Option(ZMsg.recvMsg(socket, ZMQ.DONTWAIT | ZMQ.NOBLOCK))
+          msg.foreach(receiver ! _)
+          if (msg.isDefined) receiveAllAvailable() else ()
+        }
 
-      ctx.self ! DoReceive
+        ctx.self ! DoReceive
 
-      Behaviors.receiveMessage {
-        case DoReceive =>
-          receiveAllAvailable()
+        Behaviors.receiveMessage {
+          case DoReceive =>
+            receiveAllAvailable()
 
-          // Try again later
-          timers.startSingleTimer((), DoReceive, pollInterval)
-          Behaviors.same
-        case Stop =>
-          socket.close()
-          zmqContext.close()
-          Behavior.stopped
+            // Try again later
+            timers.startSingleTimer((), DoReceive, pollInterval)
+            Behaviors.same
+
+          case Stop =>
+            socket.close()
+            zmqContext.close()
+            Behavior.stopped
+        }
       }
     }
-  }
 
-  sealed trait Command
-  case object DoReceive extends Command
-  case object Stop      extends Command
-}
-
-import akka.actor.typed.scaladsl.adapter._
-
-object ZeroMqSource {
-  def apply(url: String, port: Int, envelopes: Seq[String], pollInterval: FiniteDuration = 50.millis, bufferSize: Int = 100)(implicit ec: ExecutionContext,
-                                                                                                                             actorSystem: ActorSystem): Source[ZMsg, () => Unit] =
-    Source.actorRef(bufferSize, OverflowStrategy.fail).mapMaterializedValue { sourceActor =>
-      val actor = actorSystem.spawnAnonymous(ZeroMqSourceActor.behavior(url, port, envelopes, pollInterval, sourceActor.toTyped[ZMsg]))
-      () =>
-        actor ! Stop
-    }
+  private sealed trait Command
+  private case object DoReceive extends Command
+  private case object Stop      extends Command
 }
